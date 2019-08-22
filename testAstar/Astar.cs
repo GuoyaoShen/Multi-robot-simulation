b15e8ccee73791b1adb6ebfa002404e0d854b305
Guoyao Shen <48630546+GuoyaoShen@users.noreply.github.com>
2019-08-22 02:48:20 -0400

    Add scripts

---
+++ testAstar/Astar.cs
@@ -0,0 +1,534 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testAstar
+{
+    public class AstarNode  //【类】定义点，查看属性
+    {
+        private AstarNode parent = null;
+        private double g;
+        private double h;
+        private int x;
+        private int y;
+
+        public AstarNode Parent  //属性Parent
+        {
+            get
+            {
+                return parent;
+            }
+            set
+            {
+                parent = value;
+            }
+        }
+
+        public double G  //属性G
+        {
+            get
+            {
+                return g;
+            }
+            set
+            {
+                g = value;
+            }
+        }
+
+        public double H  //属性H
+        {
+            get
+            {
+                return h;
+            }
+            set
+            {
+                h = value;
+            }
+        }
+
+        public double F  //属性F
+        {
+            get
+            {
+                return g + h;
+            }
+        }
+
+        public int X  //属性X
+        {
+            get
+            {
+                return x;
+            }
+            set
+            {
+                x = value;
+            }
+        }
+
+        public int Y  //属性Y
+        {
+            get
+            {
+                return y;
+            }
+            set
+            {
+                y = value;
+            }
+        }
+
+        public AstarNode(int _x, int _y)  //自定义构造函数
+        {
+            this.x = _x;
+            this.y = _y;
+            this.parent = null;
+            this.g = 0;
+            this.h = 0;
+        }
+    }
+
+    public class Astar  //【类】算法
+    {
+        private List<AstarNode> openList = new List<AstarNode>();
+        private List<AstarNode> closeList = new List<AstarNode>();
+        private int[,] mapData;
+        private int pixelFormat = 16;
+        private int mapWidth = 0;
+        private int mapHeight = 0;
+        private int endX = 0;
+        private int endY = 0;
+
+        public int[,] MapData  //【属性】MapData
+        {
+            get
+            {
+                return mapData;
+            }
+        }
+
+        public int PixelFormat  //【属性】PixelFormat
+        {
+            get
+            {
+                return pixelFormat;
+            }
+        }
+
+        public int MapWidth  //【属性】MapWidth
+        {
+            get
+            {
+                return mapWidth;
+            }
+        }
+
+        public int MapHeight  //【属性】MapHeight
+        {
+            get
+            {
+                return mapHeight;
+            }
+        }
+
+        public Astar()  //构造函数
+        {
+        }
+
+        private bool isValid(int x, int y)  //【方法】判断点是否在地图中
+        {
+            if (x < 0 || x >= mapWidth)
+            {
+                return false;
+            }
+
+            if (y < 0 || y >= mapHeight)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool inList(List<AstarNode> list, int x, int y)  //【方法】判断点是否在列表中
+        {
+            foreach (AstarNode node in list)
+            {
+                if (node.X == x && node.Y == y)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool inOpenList(int x, int y)  //【方法】判断点是否在开启列表中
+        {
+            return inList(openList, x, y);
+        }
+
+        private bool inCloseList(int x, int y)  //【方法】判断点是否在关闭列表中
+        {
+            return inList(closeList, x, y);
+        }
+
+        private AstarNode getBestNodeFromOpenList()  //【方法】 取最优点(开启列表第一个元素)
+        {
+            if (openList.Count == 0)
+            {
+                return null;
+            }
+
+            return openList[0];  //取开启列表第一个元素
+        }
+
+        private void openToClose(AstarNode node)  //【方法】 开启列表 转到 关闭列表
+        {
+            openList.Remove(node);
+            closeList.Add(node);
+        }
+
+        private AstarNode openToCloseWithBest()  //【方法】 将最优点由 开启列表 转到 关闭列表
+        {
+            AstarNode node = getBestNodeFromOpenList();
+
+            if (node == null)
+            {
+                return null;
+            }
+
+            openToClose(node);
+            return node;
+        }
+
+        private void addToOpenList(AstarNode parent, int x, int y)  //【方法】 将点加入开启列表（以parent为父节点在开启列表中加入点（x，y））【包含估价函数】
+        {
+            int absX = Math.Abs(endX - x);
+            int absY = Math.Abs(endY - y);
+
+            if (!isValid(x, y))  //是否在地图中
+            {
+                return;
+            }
+
+            if (inOpenList(x, y) || inCloseList(x, y))  //是否在列表中
+            {
+                return;
+            }
+
+            if (!canWalk(x, y) && parent != null)  //是否可走或有父节点
+            {
+                return;
+            }
+
+            AstarNode node = new AstarNode(x, y);
+            node.Parent = parent;
+
+            if (parent == null)
+            {
+                node.G = 0;
+                node.H = 0;
+            }
+            else
+            {
+                if (Math.Abs(parent.X - x) + Math.Abs(parent.Y - y) == 2)
+                {
+                    node.G = 14;
+                }
+                else
+                {
+                    node.G = 10;
+                }
+
+                node.H = 10*((Math.Abs(endX - x)  + Math.Abs(endY - y)) +  (1.414 - 2)*Math.Min(absX, absY));  //预估H：切比雪夫距离
+            }
+
+            openList.Add(node);
+            openList.Sort(delegate(AstarNode lhs, AstarNode rhs)  //方法 按 lhs比rhs大的 升序排列
+            {
+                if (lhs.F < rhs.F)
+                {
+                    return -1;
+                }
+                else if (lhs.F > rhs.F)
+                {
+                    return 1;
+                }
+                return 0;
+            });
+        }
+
+        private void genAroundNode(AstarNode node)  //【方法】 加入相邻点，八点以及直角障碍判别
+        {
+            int x = node.X;
+            int y = node.Y;
+            addToOpenList(node, node.X - 1, node.Y);
+
+            addToOpenList(node, node.X, node.Y - 1);
+            addToOpenList(node, node.X, node.Y + 1);
+
+            addToOpenList(node, node.X + 1, node.Y);
+            
+            
+            if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1))  //1
+            {
+                addToOpenList(node, node.X + 1, node.Y - 1);
+                addToOpenList(node, node.X + 1, node.Y + 1);
+            }
+            else if (canWalk(x - 1, y) && !canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1))  //2
+            {
+                addToOpenList(node, node.X - 1, node.Y - 1);
+                addToOpenList(node, node.X + 1, node.Y - 1);
+            }
+            else if (canWalk(x - 1, y) && canWalk(x, y + 1) && !canWalk(x + 1, y) && canWalk(x, y - 1))  //3
+            {
+                addToOpenList(node, node.X - 1, node.Y - 1);
+                addToOpenList(node, node.X - 1, node.Y + 1);
+            }
+            else if (canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && !canWalk(x, y - 1))  //4
+            {
+                addToOpenList(node, node.X - 1, node.Y + 1);
+                addToOpenList(node, node.X + 1, node.Y + 1);
+            }
+            else if (!canWalk(x - 1, y) && !canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1))  //5
+            {
+                addToOpenList(node, node.X + 1, node.Y - 1);
+            }
+            else if (canWalk(x - 1, y) && !canWalk(x, y + 1) && canWalk(x + 1, y) && !canWalk(x, y - 1))  //6
+            {
+
+            }
+            else if (canWalk(x - 1, y) && !canWalk(x, y + 1) && !canWalk(x + 1, y) && canWalk(x, y - 1))  //7
+            {
+                addToOpenList(node, node.X - 1, node.Y - 1);
+            }
+            else if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && !canWalk(x, y - 1))  //8
+            {
+                addToOpenList(node, node.X + 1, node.Y + 1);
+            }
+            else if (!canWalk(x - 1, y) && canWalk(x, y + 1) && !canWalk(x + 1, y) && canWalk(x, y - 1))  //9
+            {
+
+            }
+            else if (canWalk(x - 1, y) && canWalk(x, y + 1) && !canWalk(x + 1, y) && !canWalk(x, y - 1))  //10
+            {
+                addToOpenList(node, node.X - 1, node.Y + 1);
+            }
+            else if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1)) { }
+            else if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1)) { }
+            else if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1)) { }
+            else if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1)) { }
+            else if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1)) { }  //15  全是障碍
+            else  //16  没有障碍
+            {
+                addToOpenList(node, node.X - 1, node.Y - 1);
+                addToOpenList(node, node.X - 1, node.Y + 1);
+                addToOpenList(node, node.X + 1, node.Y - 1);
+                addToOpenList(node, node.X + 1, node.Y + 1);
+            }
+            //if (!canWalk(x - 1, y) && canWalk(x, y + 1) && canWalk(x + 1, y) && canWalk(x, y - 1)) { }
+            
+            
+
+        }
+
+        private AstarNode findNearPointFromList(List<AstarNode> list, int x, int y)  //【方法】找列表中到点（x，y）之切比雪夫距离最小点
+        {
+            
+            AstarNode result = null;
+            double minDistance = int.MaxValue;  //MaxValue 一个特大值
+
+            foreach (AstarNode node in list)
+            {
+                //【下面距离公式用作开启列表节点筛选的引导】
+                double dist = node.F;
+
+                if (dist < minDistance)
+                {
+                    minDistance = dist;
+                    result = node;
+                }
+            }
+
+            return result;
+        }
+
+        public bool canWalk(int x, int y)  //【方法】判断点(x,y)是否可走
+        {
+            if (mapData[x, y] < -1)  //路为-2。判断为路，则可用
+                return true;
+            else  //其他，为固定障碍或机器人
+                return false;
+        }
+
+        public bool canWalkPixel(int x, int y)  //【方法】判断像素点(x,y)是否可走
+        {
+            int px = x / pixelFormat;
+            int py = y / pixelFormat;
+
+            return canWalk(px, py);
+        }
+
+        public List<AstarNode> findPath(int _startX, int _startY, int _endX, int _endY)  //【方法】找路
+        {
+            this.endX = _endX;
+            this.endY = _endY;
+            this.openList.Clear();
+            this.closeList.Clear();
+            List<AstarNode> result = new List<AstarNode>();
+            AstarNode currNode = null;
+            bool findPathFlag = false;
+
+
+            if (canWalk(endX, endY))  //添加。 防止因终点不可用造成崩溃
+                                      //终点可用
+            {
+                addToOpenList(null, _startX, _startY);  //此时Parent==null
+
+                while (openList.Count > 0)
+                {
+                    currNode = openToCloseWithBest();  //将最优点(开启列表第一个)由开启列表转到关闭列表
+
+                    if (currNode == null)
+                    {
+                        break;
+                    }
+
+                    if (currNode.X == _endX && currNode.Y == _endY)
+                    {
+                        findPathFlag = true;
+                        break;
+                    }
+
+                    genAroundNode(currNode);
+                }
+
+                if (!findPathFlag)
+                {
+                    currNode = findNearPointFromList(closeList, endX, endY);  //此为欧几里得距离判断，减少不必要点的判断
+                }
+
+                if (currNode == null)
+                {
+                    return null;
+                }
+
+                while (true)
+                {
+                    result.Add(currNode);
+
+                    if (currNode.X == _startX && currNode.Y == _startY)
+                    {
+                        break;
+                    }
+
+                    currNode = currNode.Parent;
+                }
+
+                result.Reverse();
+
+                return result;
+            }
+            else  //终点不可用
+            {
+                result = null;  //停止循迹
+                return result;
+            }
+
+        }
+
+        public List<AstarNode> findPathPixel(int startX, int startY, int endX, int endY)  //【方法】找路 像素
+        {
+            int sx = startX / pixelFormat;
+            int sy = startY / pixelFormat;
+            int ex = endX / pixelFormat;
+            int ey = endY / pixelFormat;
+
+            List<AstarNode> result = findPath(sx, sy, ex, ey);
+
+            if (result == null)
+            {
+                return null;
+            }
+
+            for (int i = 0; i < result.Count; ++i)
+            {
+                result[i].X *= pixelFormat;
+                result[i].Y *= pixelFormat;
+            }
+
+            return result;
+        }
+
+        public void enableMapData(int x, int y, int value)  //【方法】使能点(x,y)
+        {
+            mapData[x, y] = value;
+        }
+
+        public void enableMapDataPixel(int x, int y, int value)  //【方法】使能像素点(x,y)
+        {
+            int px = x / pixelFormat;
+            int py = y / pixelFormat;
+
+            enableMapData(px, py, value);
+        }
+
+        public void enableMapDataAll(int value)  //【方法】使能全地图点(x,y)
+        {
+            for (int w = 0; w < mapWidth; ++w)
+            {
+                for (int h = 0; h < mapHeight; ++h)
+                {
+                    mapData[w, h] = value;
+                }
+            }
+        }
+
+        public void initMapData(int _widthPixel, int _heightPixel, int _pixelFormat)  //【方法】初始化地图（全图可走）
+        {
+            int width = _widthPixel / _pixelFormat;
+            int height = _heightPixel / _pixelFormat;
+
+            pixelFormat = _pixelFormat;
+            mapData = new int[width, height];
+            mapWidth = width;
+            mapHeight = height;
+
+            enableMapDataAll(-2);  //全部初始化为可走点
+        }
+
+        public int checkAround(int x, int y)  //【方法】新添加方法。用于检查[x, y]周围八个点的情况。
+        {
+            int result;  //用于存放周围八个点的最大值
+            result = -3;
+            int cx = x / pixelFormat;
+            int cy = y / pixelFormat;
+            for (int i = cy - 1;i <= cy + 1; i++)
+            {
+                if (mapData[cx - 1, i] > result)
+                   result = mapData[cx - 1, i];
+            }
+            for (int i = cy - 1; i <= cy + 1; i+=2)
+            {
+                if (mapData[cx, i] > result)
+                   result = mapData[cx, i];
+            }
+            for (int i = cy - 1; i <= cy + 1; i ++)
+            {
+                if (mapData[cx + 1, i] > result)
+                   result = mapData[cx + 1, i];
+            }
+            if (result <= 0)  //为路或者固定障碍或者闲置机器人
+                return 0;
+            else if ((result > 0) && (result < mapData[cx, cy]))  //有机器人但是编号比自己小（自己先走）
+                return -1;
+            else  //有机器人且编号比自己大（对方先走）
+                return 1;
+        }
+    }
+}
---
+++ testAstar/Form1.cs
@@ -0,0 +1,289 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testAstar
+{
+    public class Player  //【类】定义对单个机器人的一类寻路操作流程
+    {
+        public int ID;
+        public int X;  //X,Y表示Player当前位置坐标
+        public int Y;
+        public int TargetX;
+        public int TargetY;
+        public List<AstarNode> Paths;
+        public int PathIndex;  //指示 List<AstarNode> Paths 中元素编号（index“指标”）
+        public Color PathColor;
+        public Color ShowColor;
+
+        public void delete(Astar astar)  //【方法】删除机器人后使能当前点
+        {
+            astar.enableMapDataPixel(X, Y, -2);
+        }
+
+        public void update(Astar astar)  //【方法】寻路及再次自更新
+        {
+            if (Paths != null)  //有路
+            {
+                if (PathIndex < Paths.Count)  //如果没走完。（PathIndex初始为0）
+                {
+                    if (astar.checkAround(X, Y) <= 0)  //周围无机器人（0），或者有编号小于自己的机器人（-1），则自己正常走if(astar.checkAround(X, Y) <= 0)
+                    {
+                        int tx = Paths[PathIndex].X;
+                        int ty = Paths[PathIndex].Y;
+                        
+                        if (astar.canWalkPixel(tx, ty))  //判断下一步点为可走
+                        {
+                            astar.enableMapDataPixel(X, Y, -2);  //走离当前点，当前点置为可用
+                            X = tx;  //走到下一点
+                            Y = ty;
+                            astar.enableMapDataPixel(X, Y, ID);  //新走到的点置为不可用.编号为自己的编号
+                            PathIndex++;
+
+                            if(astar.canWalkPixel(TargetX, TargetY))  //只在终点可用时进行动态刷新，防止因重点不可用带来的崩溃。
+                            {
+                                Paths = astar.findPathPixel(X, Y, TargetX, TargetY); //【动态规划】每一步都重新刷新轨迹。
+                                PathIndex = 1;
+                            }
+                        }
+                        else  //无可走点则刷新
+                        {
+                            Paths = astar.findPathPixel(X, Y, TargetX, TargetY);
+                            PathIndex = 1;
+                        }
+                    }
+                    else  //周围有编号比自己大的机器人，则暂停行动，且重新刷新路径
+                    {
+                        Paths = astar.findPathPixel(X, Y, TargetX, TargetY);
+                        PathIndex = 1;
+                    }
+                }
+                else  //列表计数指标到了（当前目标全走完了）则刷新
+                {
+                    astar.enableMapDataPixel(X, Y, -1);  //原为true
+                    Paths = null;
+                    PathIndex = 0;
+                }
+            }
+            else  //已经停止行进(Paths==null)
+            {
+                astar.enableMapDataPixel(X, Y, -1);  //机器人静止则赋予当前位置优先级最低编号，保证其他优先级低于当前位置的机器人能正常在周围移动
+            }
+        }
+
+        public void render(Astar astar, Graphics g)  //【方法】路径着色及机器人编号标记
+        {
+            if (Paths != null)
+            {
+                for (int i = PathIndex; i < Paths.Count; ++i)
+                {
+                    g.FillRectangle(new SolidBrush(PathColor), new Rectangle(Paths[i].X, Paths[i].Y, astar.PixelFormat, astar.PixelFormat));  //路径着色
+                }
+            }
+
+            g.FillRectangle(new SolidBrush(ShowColor), new Rectangle(X, Y, astar.PixelFormat, astar.PixelFormat));  //当前位置(X,Y)着色
+
+            g.DrawString(ID.ToString(), new Font("楷体", 14, FontStyle.Bold), Brushes.Black, X, Y);
+        }
+    }
+
+    public partial class Form1 : Form  //【类】GUI
+    {
+        public static int MapWidth = 640;  //原640
+        public static int MapHeight = 480;  //原480
+
+        public static Random rand = new Random((int)DateTime.Now.Ticks);
+
+        private Astar astar = new Astar();
+        private Bitmap surface = null;
+        private Graphics g = null;
+
+        private int NewTargetX;
+        private int NewTargetY;
+
+        private List<Player> players = new List<Player>();
+
+        private bool[] keys = new bool[256];
+
+        private void init()  //【方法】 初始化，更改方块大小
+        {
+            pictureBox1.Location = Point.Empty;  //一种GDI控件pictureBox
+            pictureBox1.ClientSize = new System.Drawing.Size(MapWidth, MapHeight);
+
+            surface = new Bitmap(MapWidth, MapHeight);
+            g = Graphics.FromImage(surface);
+
+            astar.initMapData(MapWidth, MapHeight, 16);  //此处修改最后一个参数pixelFormat(原：16)可修改方块大小
+
+            for (int i = 0; i < keys.Length; ++i)
+            {
+                keys[i] = false;
+            }
+        }
+
+        private void update()  //【方法】全机器人更新
+        {
+            foreach (Player p in players)
+            {
+                p.update(astar);
+            }
+        }
+
+        private void render()  //【方法】着色
+        {
+            g.Clear(Color.White);
+
+            int[,] mapData = astar.MapData;
+
+            for (int w = 0; w < astar.MapWidth; ++w)  //障碍着色
+            {
+                for (int h = 0; h < astar.MapHeight; ++h)
+                {
+                    if (mapData[w, h]==0)
+                    {
+                        g.FillRectangle(Brushes.Black, new Rectangle(w * astar.PixelFormat, h * astar.PixelFormat, astar.PixelFormat, astar.PixelFormat));
+                    }
+                }
+            }
+
+            foreach (Player p in players)  //机器人着色
+            {
+                p.render(astar, g);
+            }
+
+            pictureBox1.Image = surface;
+        }
+
+        public Form1()  //【方法】初始化GUI界面
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)  //【方法】窗口标签设置，更改行进速度
+        {
+            this.Text = "多机器人协作演示 A:增加 D:减少 左键:障碍设置 右键+数字键:对应编号物体的寻路 右键+0键:指定任务分配坐标点";
+            init();
+
+            Timer gameTimer = new Timer();  //新建计时器gameTimer
+            gameTimer.Tick += gameTimer_Tick;
+            gameTimer.Interval = 500;  //改此数值可以改变行进速度（即刷新速度）
+            gameTimer.Start();
+        }
+
+        void gameTimer_Tick(object sender, EventArgs e)  //【方法】刷新事件，间隔gameTimer.Interval = 100（ms），见168行
+        {
+            update();
+            render();
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)  //【方法】鼠标
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)  //鼠标左键
+            {
+                astar.enableMapDataPixel(e.X, e.Y, 0);  //设置为障碍
+            }
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)  //鼠标右键
+            {
+                /*endX = e.X;
+                endY = e.Y;
+                paths = astar.findPathPixel(px, py, endX, endY);
+                pathIndex = 0;*/
+
+                int pi = 0;
+                for (int i = 0; i < 256; ++i)
+                {
+                    if (keys[i])  //keys布尔一维数组
+                    {
+                        pi = i - (int)Keys.D1;
+
+                        if (pi < 0 || pi >= players.Count)
+                        {
+                            if(pi == -1)  //0键+鼠标右键指定任务分配坐标点
+                            {
+                                NewTargetX = e.X;
+                                NewTargetY = e.Y;
+
+                                int step = 999;
+                                int stepEstimate = 999;  //用作路径步数计量。
+                                int id = 0;
+                                int j;
+                                for ( j = 0; j < players.Count; j++)  //检索所有机器人
+                                {
+                                    if (astar.MapData[(players[j].X) / astar.PixelFormat, (players[j].Y) / astar.PixelFormat] == -1)  //发现闲置机器人
+                                    {
+                                        stepEstimate = astar.findPathPixel(players[j].X, players[j].Y, NewTargetX, NewTargetY).Count;  //以到达的步数为调用判据。
+                                        if(stepEstimate < step)
+                                        {
+                                            step = stepEstimate;
+                                            id = j;
+                                        }
+                                    }
+                                }
+                                players[id].TargetX = NewTargetX;
+                                players[id].TargetY = NewTargetY;
+                                players[id].Paths = astar.findPathPixel(players[id].X, players[id].Y, players[id].TargetX, players[id].TargetY);
+                                players[id].PathIndex = 1;
+                            }
+                            return;
+                        }
+
+                        else
+                        {
+                            Player p = players[pi];
+
+                            p.TargetX = e.X;
+                            p.TargetY = e.Y;
+                            p.Paths = astar.findPathPixel(players[pi].X, players[pi].Y, e.X, e.Y);
+                            p.PathIndex = 1;  //原为0
+
+                            players[pi] = p;
+
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)  //【方法】键盘，更改初始位置
+        {
+            keys[e.KeyValue] = true;
+
+            if (e.KeyCode == Keys.A)
+            {
+                Player p = new Player();
+                p.ID = players.Count + 1;
+                p.X = astar.PixelFormat * players.Count + 64;  //此处更改机器人初始位置（16： pixelFormat）
+                p.Y = 64;
+                p.TargetX = 0;
+                p.TargetY = 0;
+                p.Paths = null;
+                p.PathIndex = 0;
+                p.ShowColor = Color.FromArgb(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255));
+                p.PathColor = Color.FromArgb(64, p.ShowColor);
+
+                players.Add(p);  //在玩家列表中添加新建玩家
+            }
+
+            if (e.KeyCode == Keys.D)
+            {
+                if (players.Count > 0)
+                {
+                    players[players.Count - 1].delete(astar);  //调用方法使能当前点
+                    players.RemoveAt(players.Count - 1);  //RemoveAt删除第(players.Count - 1)项
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)  //【方法】抬键无效
+        {
+            keys[e.KeyValue] = false;
+        }
+    }
+}
